a91bb0587e228a83ffe908e67872c6d0875716b9
heba-Abdallah <heba.essam.m@hotmail.com>
2021-06-07 19:48:50 +0300

    doing css

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AboutUs.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 class AboutUs extends React.Component {
