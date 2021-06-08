0de40219abe1e2f33c0fd960aa51e39d1be0aa40
amroalbarham <amroalbarham@gmail.com>
2021-06-08 19:26:37 +0300

    edit by sokainah

--- src/components/Fitness.js
+++ src/components/Fitness.js
@@ -36,36 +36,84 @@ class Fitness extends React.Component {
 
   render() {
     return (
-      <div className='fitness'>
+      <div className='holepage'>
+      {/* <div className='fitness'> */}
         {/* <Jumbotron> */}
-        <h1>Fitness</h1>
-        
+        {/* <h1>Fitness</h1> */}
 
-       
-        <CardColumns className='columnsfitness'>
+
+
+        {/* <CardColumns className='cardcolumn' > */}
           {this.state.displayCard &&
             this.state.fitness.map((item, index) => {
               return (
-                <Card className='Cardfitness' style={{ width: '18rem' }}>
+                // <Card className='Cardfitness' style={{ width: '18rem' }}>
 
-                  <Card.Img variant="top" className='imagefitness' src={item.image} />
-                  <Card.Body>
-                    <Card.Title>{item.name}</Card.Title>
-                    <Card.Text>{item.description}</Card.Text>
-                    <Card.Text>{item.repeat}</Card.Text>
+                //   <Card.Img variant="top" className='imagefitness' src={item.image} />
+                //   <Card.Body>
+                //     <Card.Title>{item.name}</Card.Title>
+                //     <Card.Text>{item.description}</Card.Text>
+                //     <Card.Text>{item.repeat}</Card.Text>
 
-                  </Card.Body>
-                </Card>
+                //   </Card.Body>
+                // </Card>
+            <>
+                  <div className="fitness_container">
+                    <div className="post">
+                      <div className="header_post">
+                        <img src={item.image} alt=""/>
+        </div>
+
+                        <div className="body_post">
+                          <div className="post_content">
+
+                            <h1>{item.name}</h1>
+                            <p>{item.description}</p>
+
+                            <div className="container_infos">
+                              <div className="postedBy">
+                                <span>author</span>
+                        swift action
+                    </div>
+
+                              <div className="container_tags">
+                                <span>repeat</span>
+                                <p>{item.repeat}</p>
+                                <div className="tags">
+                                  {/* <ul>
+                                    <li>design</li>
+                                    <li>front end</li>
+                                  </ul> */}
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    {/* <a href={item.link} target="_blank"><footer>
+                      <div class="texto">
+                        <span>
+                          <i class="fab fa-youtube"></i>
+             watch on youtube</span>
+                      </div>
+                    </footer>
+                    </a> */}
+                    </>
               )
             })
-
+            
           }
 
-        </CardColumns>
+        {/* </CardColumns> */}
 
 
-        {/* </Jumbotron> */}
-      </div>
+        {/* </Jumbotron> */ }
+      {/* </div> */}
+          </div>
     )
   }
 }
