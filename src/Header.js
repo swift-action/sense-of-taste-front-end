fc1115c357223bf60ca2d549327862abb02ede43
Batoolayyad <batoolayyad1996@gmail.com>
2021-06-08 19:21:01 +0300

    home page, last edit

--- src/Header.js
+++ src/Header.js
@@ -6,26 +6,20 @@ import './header.css';
 import LogoutButton from './components/LogoutButton';
 
 
-
-
-
-
-
 class Header extends React.Component {
   render() {
     return(
-      <Navbar  collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand> Sense of test</Navbar.Brand>
-         
-          <Link className='navBar' to="/">Home</Link>
-          <Link className='navBar' to="/Profile">Profile</Link>
-          <Link className='navBar' to="/Foods">Recipes</Link>
-          <Link className='navBar' to="/Fitness">Fitness</Link>
-          <Link className='navBar' to="/AboutUs">About Us</Link>
-          
-          <LogoutButton className='logout-button'  />
-         
-      </Navbar>
+      <>
+       <Navbar  collapseOnSelect expand="lg" bg="dark" variant="dark">
+         <Navbar.Brand> Sense of test</Navbar.Brand>
+           <Link className='navBar' to="/">Home</Link>
+           <Link className='navBar' to="/Profile">Profile</Link>
+           <Link className='navBar' to="/Foods">Recipes</Link>
+           <Link className='navBar' to="/Fitness">Fitness</Link>
+           <Link className='navBar' to="/AboutUs">About Us</Link>
+           <LogoutButton className='logout-button'  /> 
+        </Navbar>
+</>
     )
   }
 }
